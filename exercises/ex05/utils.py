d18b88c800f3a66ad3b917fb8b624cfad0620ad8
Nicholas Mountain <nmountain@unc.edu>
2022-02-23 22:25:31 -0500

    utils progress

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,33 @@
+"""Docstring."""
+
+__author__ = "730477365"
+
+
+def only_evens(numbers: list[int]) -> list[int]:
+    new_list: list[int] = list()
+    for i in numbers:
+        if i % 2 == 0:
+            new_list.append(i)
+    return new_list
+
+
+def sub(numbers: list[int], start, end) -> list[int]:
+    new_list: list[int] = list()
+    i: int = start
+    if start < 0:
+        i: int = 0
+    if end > len(numbers):
+        end = len(numbers)
+    while start <= i < end:
+        new_list.append(numbers[i])
+        i += 1
+    return new_list
+
+
+def concat(list_1: list[int], list_2: list[int]) -> list[int]:
+    new_list: list[int] = list()
+    for i in list_1:
+        new_list.append(i)
+    for alt_i in list_2:
+        new_list.append(alt_i)
+    return new_list
